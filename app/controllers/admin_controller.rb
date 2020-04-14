f96bf5e0c550b335c1079c1a1238156a6f1968e8
remeal <dglibchenko@mail.ru>
2020-04-15 02:52:20 +0300

    Done all logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,12 +5,16 @@ class AdminController < ApplicationController
 
   def add_new_guests
     @booking = Type.find_by_id(Room.find_by_number(Booking.find_by_id(params[:add_guests])[:room_id])[:type_id])[:quantity]
+    flash[:add_guests] =  params[:add_guests]
   end
 
   def success_to_add
-    booking = params[:type_value]
+    for i in 1..params[:type_value].to_i
+      Guest.create(password: params[:passport.to_s + i.to_s], surname: params[:surname.to_s + i.to_s],
+                   name: params[:name.to_s + i.to_s], patronymic: params[:patronymic.to_s + i.to_s], booking: Booking.find_by_id(flash[:add_guests]))
+    end
   end
   def guests
-
+    @guests = Guest.all
   end
 end
